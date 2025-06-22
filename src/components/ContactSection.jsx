f97cac028800f47f34f3d30542ee2de1688d9964
William Wagner <williamwagner@epitech.eu>
2025-06-22 21:47:57 +0200

    add id attribute to ContactSection for improved accessibility

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -19,7 +19,7 @@ export const ContactSection = () => {
         }, 1500);
     }
 
-    return <section className="py-24 px-4 relative bg-secondary/30">
+    return <section className="py-24 px-4 relative bg-secondary/30" id ="contact">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Get In <span className="text-primary">Touch</span></h2>
             <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto ">
