e2565db7279d33611b83967f21f4f22bc271400d
William Wagner <williamwagner@epitech.eu>
2025-06-26 11:20:35 +0200

    fix: update LinkedIn link to include proper URL format

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -72,7 +72,7 @@ export const ContactSection = () => {
                     <div className="pt-8 ">
                         <h4 className="font-medium mb-4">Connect With Me</h4>
                         <div className="flex space-x-4 justify-center">
-                            <a href="www.linkedin.com/in/william-wagner-87ab3722b" target="_blank">
+                            <a href="https://www.linkedin.com/in/william-wagner-87ab3722b/" target="_blank">
                                 <Linkedin />
                             </a>
                             <a href="https://www.instagram.com/william_w67_/profilcard" target="_blank">
