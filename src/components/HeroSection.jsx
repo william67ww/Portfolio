6d1863a218611ebc9dbb3b2f0bfd9fa29decd1c3
william67ww <william.wagner@epitech.eu>
2025-07-05 21:49:09 +0200

    Update HeroSection.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -22,7 +22,7 @@ export const HeroSection = () => {
         <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
             <div className="container max-w-4xl mx-auto text-center z-10">
                 <div className="space-y-6 relative">
-                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight relative">
+                    <h1 className="text-2xl md:text-6xl font-bold tracking-tight relative">
                         <span className="relative">
                             <AnimatedLetters text="Hi, I'm" delayStart={0} /> <Sparkle />
                         </span>
